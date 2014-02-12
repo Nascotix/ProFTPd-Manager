9038d8cf93421e84e5c29ca639e601c84a850965
Yann <yann@igloo.be>
2014-02-12 17:05:24 +0100

    Modification des formulaires en édition, validation serveur

---
+++ node/gloo/test/test.js
@@ -0,0 +1,16 @@
+
+describe('description', function () {
+  it('description', function (done) {
+    request(app)
+      .expect()
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.
+
+        done()
+      })
+  });
+});
